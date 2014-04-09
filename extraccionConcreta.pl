1e0ae3573230b87f4d7bf6a7ee36546cd501e064
oskyar <oskyar@gmail.com>
2014-04-09 17:46:35 +0200

    Añadidos los archivos que empiezan por 03

--- extraccionConcreta.pl
+++ extraccionConcreta.pl
@@ -61,6 +61,18 @@ while(my $nombre_fichero = shift @entrada){
     $nombre_fichero =~ /(\d+\.[dD][aA][tT])/;
     #Se cogen las dos primeras cifras que es el identificador
     switch(substr($1,0,2)){
+        case "03"{
+            print SALIDA '"Tipo de elección.","Año del proceso electoral.", "Mes del proceso electoral.","Código de la candidatura.", "Siglas de la candidatura.", "Denominación de la candidatura.", "Código de la candidatura cabecera de acumulación a nivel provincial.", "Código de la candidatura cabecera de acumulación a nivel autonómico.", "Código de la candidatura cabecera de acumulación a nivel nacional."'."\n";
+            foreach my $linea (<ENTRADA>)  {
+                if((substr($linea, $inicio-1,$longitud) == $valor) && scalar(@ARGV) == 3){
+                    print SALIDA substr($linea, 0,2) . "," . substr($linea, 2,4) . "," . substr($linea, 6,2) . "," . substr($linea, 8,6) . "," . substr($linea, 14,50) . "," . substr($linea, 64,150) . "," . substr($linea, 214,6) . "," . substr($linea, 220,6) . "," . substr($linea, 226,6) . "\n";
+                }else{
+                    if(scalar(@ARGV) == 0){
+                        print SALIDA substr($linea, 0,2) . "," . substr($linea, 2,4) . "," . substr($linea, 6,2) . "," . substr($linea, 8,6) . "," . substr($linea, 14,50) . "," . substr($linea, 64,150) . "," . substr($linea, 214,6) . "," . substr($linea, 220,6) . "," . substr($linea, 226,6) . "\n";
+                    }
+                }
+            }
+        }
         case "09" {
             print SALIDA '"Tipo de elección.","Año del proceso electoral.","Mes del proceso electoral.","Número de vuelta (en procesos a una sola vuelta = 1) o Número de pregunta en Referéndum.","Código de la Comunidad Autónoma o 99 si se trata del Total Nacional del C.E.R.A.","Código I.N.E. de la provincia o 99 si se trata del Total Nacional o Autonómico del C.E.R.A.","Código I.N.E. del municipio (999 = C.E.R.A.).","Número de distrito municipal en su caso o 01 si el municipio no tiene distritos (distrito único). En el caso de datos procedentes del C.E.R.A., llevará el número del ‘Distrito Electoral’ a que correspondan o 09 si el ámbito de dicho distrito coincide con el de la provincia.","Código de la sección (tres dígitos seguidos de un espacio, letra mayúscula u otro dígito).","Código de la mesa (una letra mayúscula identificando la mesa o una ‘U’ en caso de mesa única).","Censo del I.N.E.","Censo de escrutinio o censo C.E.R.A.","Censo C.E.R.E. en escrutinio (Residentes Extranjeros).","Total votantes C.E.R.E. (Residentes Extranjeros).","Votantes del primer avance de participación.","Votantes del segundo avance de participación.","Votos en blanco.","Votos nulos.","Votos a candidaturas.","Votos afirmativos en Referéndum o ceros en otros procesos electorales.","Votos negativos en Referéndum o ceros en otros procesos electorales.","Datos oficiales (Si/No)."'."\n";
             foreach my $linea (<ENTRADA>)  {
